8f0f9b0b3d369fd13f389243f6d80cb7b449751b
karthika525 <karthikaindrajith@gmail.com>
2025-09-22 23:50:57 +0530

    sign up form to get daily quotes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const indexRouter = require('./routes/index');
+const db = require('./database/db')
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const User = require('../models/userModel');
+
 router.get('/', (req, res) => {
-  res.render('reg-form', {
-    errors: [],oldData: {}
-  });
+  res.render('sign-up', { errors: [], oldData: {} });
 });
+
 router.post('/createUser', [
-  check('name').notEmpty().withMessage('Name is required'),
+  check('username').notEmpty().withMessage('Username is required'),
   check('email').isEmail().withMessage('Invalid email address'),
   check('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.render('reg-form', {
+    return res.render('sign-up', {
       errors: errors.array(),
       oldData: req.body
     });
   }
 
-  // if no validation errors
-  const { name, email } = req.body;
-  res.render('form-data', { name, email });
-});
+  const { username, email, password } = req.body;
 
+  try {
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.render('form-data', { message: "Thank you for signing up!", user: { username, email } });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
